6c952adf164c1bf893b16494ed5b2898d34bf181
JESHAR23 <jessedahart@gmail.com>
2025-10-15 23:07:34 +0200

    new trail 2

--- src/Login.java
+++ src/Login.java
@@ -3,18 +3,22 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import static javax.swing.BoxLayout.LINE_AXIS;
-
 public class Login implements ActionListener {
 
-    JFrame loginpage = new JFrame();
+    JFrame Loginpage = new JFrame();
 
-    JPanel btnlayout = new JPanel();
-    JPanel detaillayout = new JPanel();
-    JPanel imagelogo = new JPanel();
+    JPanel pnlsouth = new JPanel();
+    JPanel pnlcentre = new JPanel();
+    JPanel pnlnorth = new JPanel();
+    JPanel pnleast = new JPanel();
+    JPanel pnlwest = new JPanel();
+    JPanel pnlsouthsouth = new JPanel();
+    JPanel pnlnorthsouth = new JPanel();
 
     JLabel lblName = new JLabel("Full Name: ");
     JLabel lblPass = new JLabel("Password : ");
+
+    JLabel companyimage = new JLabel();
 
     JTextField txtName = new JTextField();
     JTextField txtPass = new JTextField();
@@ -26,54 +30,87 @@ public class Login implements ActionListener {
 
     JButton btnLogin = new JButton("Login");
     JButton btnSignup = new JButton("Signup");
+    JButton btnForgotpass = new JButton("Forgot Password");
 
-    ImageIcon logoicon = new ImageIcon("logo.png");
+    ImageIcon logoicon = new ImageIcon("logoinverted.png");
+    ImageIcon smallicon = new ImageIcon("logo.png");
 
     Login(){
-        loginpage.setVisible(true);
-        loginpage.setSize(650,350);
-        loginpage.setResizable(false);
-        loginpage.setTitle("MentCare Login");
-        loginpage.setIconImage(logoicon.getImage());
-        loginpage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Loginpage.setVisible(true);
+        Loginpage.setSize(750,450);
+        Loginpage.setResizable(false);
+        Loginpage.setTitle("MentCare Login");
+        Loginpage.setIconImage(smallicon.getImage());
+        Loginpage.setLayout(new BorderLayout(0,0));
+        Loginpage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        btnlayout.setLayout(new FlowLayout(FlowLayout.CENTER,75,25));
-        btnlayout.setBackground(Color.black);
-        detaillayout.setLayout(new GridLayout(3,2));
-        detaillayout.setBackground(Color.black);
-        loginpage.setLayout(new BorderLayout(200,100));
-        loginpage.setBackground(Color.black);
+        pnlsouthsouth.setLayout(new FlowLayout(FlowLayout.CENTER,100,10));
+        pnlnorthsouth.setLayout(new FlowLayout(FlowLayout.CENTER,100,10));
+        pnlsouth.setLayout(new GridLayout(2,1));
+        pnlcentre.setLayout(new GridLayout(6,2,10,15));
 
-        btnLogin.setSize(75,30);
-        btnLogin.setVisible(true);
-        btnLogin.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        btnLogin.setBackground(Color.white);
-        btnLogin.addActionListener(this::actionPerformed);
+        pnlnorth.setPreferredSize(new Dimension(100,100));
+        pnlsouth.setPreferredSize(new Dimension(100,55));
+        pnleast.setPreferredSize(new Dimension(100,200));
+        pnlwest.setPreferredSize(new Dimension(100,200));
+        pnlcentre.setPreferredSize(new Dimension(250,275));
 
-        btnSignup.setSize(75,30);
-        btnSignup.setVisible(true);
-        btnSignup.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        btnSignup.setBackground(Color.white);
-        btnSignup.addActionListener(this::actionPerformed);
+        pnlnorth.setBackground(Color.black);
+        pnlsouth.setBackground(Color.black);
+        pnleast.setBackground(Color.black);
+        pnlwest.setBackground(Color.black);
+        pnlcentre.setBackground(Color.black);
+
+        companyimage.setIcon(logoicon);
+        companyimage.createImage(180,75);
 
         rbStaff.setBackground(Color.black);
         rbPatient.setBackground(Color.black);
+        rbStaff.setForeground(Color.white);
+        rbPatient.setForeground(Color.white);
+        rbStaff.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        rbPatient.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
         rbGroup.add(rbStaff);
         rbGroup.add(rbPatient);
 
-        btnlayout.add(btnSignup);
-        btnlayout.add(btnLogin);
+        btnSignup.setBackground(Color.white);
+        btnLogin.setBackground(Color.white);
+        btnForgotpass.setBackground(Color.white);
+        btnForgotpass.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnSignup.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnLogin.setCursor(new Cursor(Cursor.HAND_CURSOR));
 
-        detaillayout.add(lblName);
-        detaillayout.add(txtName);
-        detaillayout.add(lblPass);
-        detaillayout.add(txtPass);
-        detaillayout.add(rbStaff);
-        detaillayout.add(rbPatient);
+        btnLogin.setPreferredSize(new Dimension(150,30));
+        btnSignup.setPreferredSize(new Dimension(150,30));
+        btnForgotpass.setPreferredSize(new Dimension(150,30));
 
-        //loginpage.add(,BorderLayout.NORTH);
-        loginpage.add(detaillayout,BorderLayout.CENTER);
-        loginpage.add(btnlayout,BorderLayout.SOUTH);
+        pnlnorth.add(companyimage);
+
+        pnlnorthsouth.add(btnLogin);
+        pnlnorthsouth.add(btnSignup);
+        pnlsouthsouth.add(btnForgotpass);
+
+        pnlsouth.add(pnlnorthsouth);
+        pnlsouth.add(pnlsouthsouth);
+
+        lblName.setForeground(Color.white);
+        lblPass.setForeground(Color.white);
+
+        pnlcentre.add(lblName);
+        pnlcentre.add(txtName);
+        pnlcentre.add(lblPass);
+        pnlcentre.add(txtPass);
+        pnlcentre.add(rbStaff);
+        pnlcentre.add(rbPatient);
+
+
+        Loginpage.add(pnlcentre,BorderLayout.CENTER);
+        Loginpage.add(pnlnorth,BorderLayout.NORTH);
+        Loginpage.add(pnlsouth,BorderLayout.SOUTH);
+        Loginpage.add(pnleast,BorderLayout.EAST);
+        Loginpage.add(pnlwest,BorderLayout.WEST);
+
     }
 
     @Override
---
+++ src/Signup.java
@@ -0,0 +1,121 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Signup implements  ActionListener {
+
+    JFrame Signuppage = new JFrame();
+
+    JPanel pnlsouth = new JPanel();
+    JPanel pnlcentre = new JPanel();
+    JPanel pnlnorth = new JPanel();
+    JPanel pnleast = new JPanel();
+    JPanel pnlwest = new JPanel();
+
+    JLabel lblName = new JLabel("Full Name: ");
+    JLabel lblPass = new JLabel("Password : ");
+    JLabel lbldob = new JLabel("Date of Birth : ");
+    JLabel lblNumber = new JLabel("Contact Number : ");
+    JLabel lblemail = new JLabel("Email : ");
+    JLabel companyimage = new JLabel();
+
+    JTextField txtName = new JTextField();
+    JTextField txtPass = new JTextField();
+    JTextField txtdob = new JTextField();
+    JTextField txtNumber = new JTextField();
+    JTextField txtemail = new JTextField();
+
+    JRadioButton rbMedAid = new JRadioButton("Medical Aid");
+    JRadioButton rbCashCard = new JRadioButton("Cash / Card");
+
+    ButtonGroup rbGroup = new ButtonGroup();
+
+    JButton btnGoBack = new JButton("Go Back");
+    JButton btnSignup = new JButton("Signup");
+
+    ImageIcon logoicon = new ImageIcon("logoinverted.png");
+    ImageIcon smallicon = new ImageIcon("logo.png");
+
+    Signup(){
+        Signuppage.setVisible(true);
+        Signuppage.setSize(750,450);
+        Signuppage.setResizable(false);
+        Signuppage.setTitle("MentCare Login");
+        Signuppage.setIconImage(smallicon.getImage());
+        Signuppage.setLayout(new BorderLayout(0,0));
+        Signuppage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        pnlsouth.setLayout(new FlowLayout(FlowLayout.CENTER,100,10));
+        pnlcentre.setLayout(new GridLayout(6,2,10,15));
+
+        pnlnorth.setPreferredSize(new Dimension(100,100));
+        pnlsouth.setPreferredSize(new Dimension(100,55));
+        pnleast.setPreferredSize(new Dimension(100,200));
+        pnlwest.setPreferredSize(new Dimension(100,200));
+        pnlcentre.setPreferredSize(new Dimension(250,275));
+
+        pnlnorth.setBackground(Color.black);
+        pnlsouth.setBackground(Color.black);
+        pnleast.setBackground(Color.black);
+        pnlwest.setBackground(Color.black);
+        pnlcentre.setBackground(Color.black);
+
+        companyimage.setIcon(logoicon);
+        companyimage.createImage(180,75);
+
+        rbMedAid.setBackground(Color.black);
+        rbCashCard.setBackground(Color.black);
+        rbMedAid.setForeground(Color.white);
+        rbCashCard.setForeground(Color.white);
+        rbMedAid.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        rbCashCard.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        rbGroup.add(rbMedAid);
+        rbGroup.add(rbCashCard);
+
+        btnSignup.setBackground(Color.white);
+        btnGoBack.setBackground(Color.white);
+        btnSignup.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnGoBack.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        btnGoBack.setPreferredSize(new Dimension(150,30));
+        btnSignup.setPreferredSize(new Dimension(150,30));
+
+        pnlnorth.add(companyimage);
+
+        pnlsouth.add(btnGoBack);
+        pnlsouth.add(btnSignup);
+
+        lblName.setForeground(Color.white);
+        lblPass.setForeground(Color.white);
+        lbldob.setForeground(Color.white);
+        lblNumber.setForeground(Color.white);
+        lblemail.setForeground(Color.white);
+
+        pnlcentre.add(lblName);
+        pnlcentre.add(txtName);
+        pnlcentre.add(lblPass);
+        pnlcentre.add(txtPass);
+        pnlcentre.add(lbldob);
+        pnlcentre.add(txtdob);
+        pnlcentre.add(lblNumber);
+        pnlcentre.add(txtNumber);
+        pnlcentre.add(lblemail);
+        pnlcentre.add(txtemail);
+        pnlcentre.add(rbMedAid);
+        pnlcentre.add(rbCashCard);
+
+
+        Signuppage.add(pnlcentre,BorderLayout.CENTER);
+        Signuppage.add(pnlnorth,BorderLayout.NORTH);
+        Signuppage.add(pnlsouth,BorderLayout.SOUTH);
+        Signuppage.add(pnleast,BorderLayout.EAST);
+        Signuppage.add(pnlwest,BorderLayout.WEST);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
